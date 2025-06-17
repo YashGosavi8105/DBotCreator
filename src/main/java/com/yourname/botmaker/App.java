a20c733c8636feb6834edddd266ae76109e6a9ba
Fish Jones <lostandy8105@gmail.com>
2025-06-17 22:58:50 +0530

    Initial Spring Boot structure for Discord Bot Maker

---
+++ src/main/java/com/yourname/botmaker/App.java
@@ -0,0 +1,13 @@
+package com.yourname.botmaker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
