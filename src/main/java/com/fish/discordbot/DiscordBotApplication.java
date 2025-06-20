3dcc15970c10abe7ae224af8f722c80325bc96d2
Fish Jones <lostandy8105@gmail.com>
2025-06-20 23:38:10 +0530

    Complete MySQL, JPA integration, disable Flyway, fix Java version issues

--- src/main/java/com/fish/discordbot/DiscordBotApplication.java
+++ src/main/java/com/fish/discordbot/DiscordBotApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+public class DiscordBotApplication 
+{
+    public static void main(String[] args) 
+    {
+        SpringApplication.run(DiscordBotApplication.class, args);
+    }
 }
+
